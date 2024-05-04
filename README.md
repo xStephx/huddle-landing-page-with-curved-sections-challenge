02cc79ad1cee783446eb18e8d3c667d09bcad421
xStephx <bojkovskistefan9@gmail.com>
2024-05-04 19:18:49 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Steph - Huddle landing page with curved sections
---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                CustomPink: 'hsl(322, 100%, 66%)',
+                CustomLightPink: 'hsl(321, 100%, 78%)',
+                CustomLightRed: 'hsl(0, 100%, 63%)',
+                CustomVeryDarkCyan: 'hsl(192, 100%, 9%)',
+                CustomVeryPaleBlue: 'hsl(207, 100%, 98%)',
+            },
+        },
+        fontFamily: {
+            Headings: ['Poppins', 'sans-serif'],
+            Body: ['Open Sans', 'sans-serif'],
+        }
+    },
+    plugins: [],
+}
