c4dcee6493cb004e6624bbb3c507a4c349a54dd4
xStephx <bojkovskistefan9@gmail.com>
2024-05-09 18:42:04 +0200

    added more comments to code, deleted some unnecessary class names, added scale on buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Script for subscription validation email
 const emailForm = document.getElementById('emailForm');
 const emailInput = document.getElementById('emailInput');
 const emailMessage = document.getElementById('emailMessage');
@@ -7,25 +8,25 @@ emailForm.addEventListener('submit', function (event) {
     const email = emailInput.value.trim();
 
     if (!email) {
-        emailInput.classList.add('border-2', 'border-red-500');
+        emailInput.classList.add('border-2', 'border-CustomLightRed');
         emailMessage.innerText = 'Enter your email please!';
         emailMessage.style.display = 'block';
         emailMessage.style.marginTop = '5px';
         emailMessage.style.marginBottom = '5px';
         emailMessage.style.fontSize = '0.8rem';
-        emailMessage.style.color = 'red';
+        emailMessage.style.color = 'hsl(0, 100%, 63%)';
         return;
     }
 
     const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     if (!isValidEmail) {
-        emailInput.classList.add('border-2', 'border-red-500');
+        emailInput.classList.add('border-2', 'border-CustomLightRed');
         emailMessage.innerText = 'Check your email please!';
         emailMessage.style.display = 'block';
         emailMessage.style.marginTop = '5px';
         emailMessage.style.marginBottom = '5px';
         emailMessage.style.fontSize = '0.8rem';
-        emailMessage.style.color = 'red';
+        emailMessage.style.color = 'hsl(0, 100%, 63%)';
         return;
     }
 
@@ -37,7 +38,7 @@ emailInput.addEventListener('input', function () {
     const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
     if (isValidEmail) {
-        emailInput.classList.remove('border-2', 'border-red-500');
+        emailInput.classList.remove('border-2', 'border-CustomLightRed');
     }
     emailMessage.innerText = '';
     emailMessage.style.display = 'none';
